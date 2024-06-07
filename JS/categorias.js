214731930dd98287eee7b8b096035c44ecc89924
CamLc <camilovicmx@gmail.com>
2024-06-06 20:22:01 -0600

    3er version

--- JS/categorias.js
+++ JS/categorias.js
@@ -3,9 +3,11 @@ let listaCategorias = document.querySelector("#listaCategorias");
         fetch('https://fakestoreapi.com/products/categories')
             .then(res => res.json())
             .then(categorias => {
+                //console.log(categorias);
                 listaCategorias.innerHTML = "";
                 categorias.forEach( (categoria,indice) => {
                     categoria = categoria.replace("'", '');
+                    //console.log("categoria:",indice, categoria);
                     listaCategorias.innerHTML += `
                     <li class="nav-item">
                         <a class="nav-link" aria-current="page"
