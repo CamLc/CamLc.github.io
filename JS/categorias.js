276eb793397635dc686d9a0f130f3bd9350cfd2d
CamLc <camilovicmx@gmail.com>
2024-06-06 20:05:36 -0600

    2da version

---
+++ JS/categorias.js
@@ -0,0 +1,17 @@
+let listaCategorias = document.querySelector("#listaCategorias");
+    let obtieneCategorias = () =>   {
+        fetch('https://fakestoreapi.com/products/categories')
+            .then(res => res.json())
+            .then(categorias => {
+                listaCategorias.innerHTML = "";
+                categorias.forEach( (categoria,indice) => {
+                    categoria = categoria.replace("'", '');
+                    listaCategorias.innerHTML += `
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page"
+                        onclick="muestraProductos('${ categoria }');"
+                        href="#">${ categoria.toUpperCase() }</a>
+                    </li> `;
+                });
+            })
+    };
